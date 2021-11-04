06286f65c2cfe014cf86092f557a8b82b1f40f46
Yazan99sh <syria.yazan19@gmail.com>
2021-11-04 17:44:09 +0200

    add secoundry splash screen

--- client/lib/module_splash/ui/screen/splash_screen.dart
+++ client/lib/module_splash/ui/screen/splash_screen.dart
@@ -1,15 +1,17 @@
 import 'package:injectable/injectable.dart';
 import 'package:mandob_moshtarayat/generated/l10n.dart';
-import 'package:mandob_moshtarayat/module_auth/authorization_routes.dart';
 import 'package:mandob_moshtarayat/module_auth/service/auth_service/auth_service.dart';
-import 'package:flutter/material.dart';
+import 'package:mandob_moshtarayat/module_main/main_routes.dart';
 import 'package:mandob_moshtarayat/utils/images/images.dart';
+import 'package:flutter/material.dart';
+
 
 @injectable
 class SplashScreen extends StatefulWidget {
   final AuthService _authService;
-  SplashScreen(this._authService);
-
+  SplashScreen(
+      this._authService,
+      );
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -17,25 +19,35 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
+    super.initState();
     WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
       _getNextRoute().then((route) {
         Navigator.of(context).pushNamedAndRemoveUntil(route, (route) => false);
       });
     });
-    super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
+    var paddingOfImage = MediaQuery.of(context).size.height * 0.20;
     return Scaffold(
-      body: SizedBox(),
+      body: Flex(
+        direction: Axis.vertical,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top:paddingOfImage,bottom: 24),
+            child: Image.asset(ImageAsset.LOGO,height: 200,width: 200,),
+          ),
+          Spacer(flex: 1,),
+          Image.asset(ImageAsset.DELIVERY_MOTOR,fit: BoxFit.cover,
+            alignment: Alignment.bottomCenter,),
+        ],
+      ),
     );
   }
 
   Future<String> _getNextRoute() async {
-    if (widget._authService.isLoggedIn) {
-      return 'MainRoutes.MAIN_SCREEN';
-    }
-    return AuthorizationRoutes.LOGIN_SCREEN;
+    await Future.delayed(Duration(seconds: 2));
+    return MainRoutes.MAIN_SCREEN;
   }
+
 }
