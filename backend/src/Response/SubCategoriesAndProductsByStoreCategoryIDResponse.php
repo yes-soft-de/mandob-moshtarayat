04ea66b5747b0a334ef020925911625122e4f8ae
habbdevsy <alhabebcenter@hotmail.com>
2021-11-11 15:30:04 +0200

    return id in SubCategoriesAndProductsByStoreCategoryIDResponse

--- backend/src/Response/SubCategoriesAndProductsByStoreCategoryIDResponse.php
+++ backend/src/Response/SubCategoriesAndProductsByStoreCategoryIDResponse.php
@@ -4,6 +4,7 @@ namespace App\Response;
 
 class SubCategoriesAndProductsByStoreCategoryIDResponse
 {
+    public $id;
     public $productCategoryName;
     public $productCategoryImage;
     public $productCategoriesLevel2;
