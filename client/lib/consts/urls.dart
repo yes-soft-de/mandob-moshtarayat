53789d21e9939f5e25e0d9563770a4e87558b88e
Yazan99sh <syria.yazan19@gmail.com>
2021-11-14 11:52:48 +0200

    change domain

--- client/lib/consts/urls.dart
+++ client/lib/consts/urls.dart
@@ -1,5 +1,5 @@
 class Urls {
-  static const String DOMAIN = 'http://138.197.186.138';
+  static const String DOMAIN = 'https://mandoob.password-please.com';
   static const String BASE_API = DOMAIN + '';
   static const String IMAGES_ROOT = DOMAIN + '/upload/';
   static const UPLOAD_API = BASE_API + '/uploadfile';
