100ceecdc4259ff4ce10e3f5e83aa26ae5161c35
habbdevsy <alhabebcenter@hotmail.com>
2021-11-28 13:11:56 +0200

    Endpoint for Get store's pending orders and open api.

---
+++ backend/src/Response/OrdersPendingForStoreResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Response;
+
+class OrdersPendingForStoreResponse
+{
+    public $id;
+
+    public $deliveryDate;
+
+    public $orderNumber;
+
+    public $detail;
+
+    public $orderType;
+
+    public $note;
+
+    public $invoiceAmount;
+}
