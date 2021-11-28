c1a477f3b26ef3094c007be591dc04586072f3dd
yazan99sh <syria.yazan19@gmail.com>
2021-11-28 18:37:14 +0200

    edit category card design

--- client/lib/module_home/ui/widget/catagories_card.dart
+++ client/lib/module_home/ui/widget/catagories_card.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 class CategoriesCard extends StatelessWidget {
   final String title;
   final String categoryId;
-  final IconData icon;
+  final String? image;
+  final IconData? icon;
   final bool selected;
   final Function(String)? onTap;
 
-  CategoriesCard({required this.title,required this.icon,required this.selected,this.onTap,required this.categoryId});
+  CategoriesCard(
+      {required this.title,
+      this.image,
+      required this.selected,
+      this.icon,
+      this.onTap,
+      required this.categoryId});
 
   @override
   Widget build(BuildContext context) {
@@ -15,27 +22,49 @@ class CategoriesCard extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
         borderRadius: BorderRadius.circular(25),
-        onTap:(){
-          if (onTap != null){
+        onTap: () {
+          if (onTap != null) {
             onTap!(categoryId);
           }
         },
         child: Container(
-          width: 100,
+          constraints: BoxConstraints(minWidth: 100),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            color:selected ? Theme.of(context).primaryColor : Theme.of(context).backgroundColor,
-          ),
+              borderRadius: BorderRadius.circular(25),
+              color: selected
+                  ? Theme.of(context).primaryColor
+                  : Theme.of(context).cardColor,
+              boxShadow: [
+                BoxShadow(
+                    color: Theme.of(context).disabledColor,
+                    offset: Offset(0.1, 0.1),
+                    spreadRadius: 0.2,
+                    blurRadius: 5),
+              ]),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                Icon(icon,size: 35,color: selected ? Colors.white : null,),
-                SizedBox(height: 4,),
-                Text(title,style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: selected ? Colors.white : null,
-                ),textAlign: TextAlign.center,),
+                icon != null ? Icon(icon,color: selected ? Colors.white : null,size: 35,) : Image.network(
+                  image ?? '',
+                  color: selected ? Colors.white : null,
+                  width: 35,
+                  height: 35,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: selected ? Colors.white : null,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ],
             ),
           ),
