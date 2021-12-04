0ad8e7e8eab5d4c9baac346f143c183a90c32001
habbdevsy <alhabebcenter@hotmail.com>
2021-12-04 18:02:51 +0200

    change (مندوب ) to )to (كابتن).

--- backend/src/Constant/LocalNotificationList.php
+++ backend/src/Constant/LocalNotificationList.php
@@ -6,11 +6,11 @@ abstract class LocalNotificationList
 {
     static $STATE_TITLE = "حالة الطلب";
 
-    static $STATE_ON_WAY_PICK_ORDER = "المندوب في طريقه إلى المتجر";
+    static $STATE_ON_WAY_PICK_ORDER = "الكابتن في طريقه إلى المتجر";
 
-    static $STATE_IN_STORE = "المندوب في المتجر";
+    static $STATE_IN_STORE = "الكابتن في المتجر";
 
-    static $STATE_ONGOING = "المندوب في طريقه إليك";
+    static $STATE_ONGOING = "الكابتن في طريقه إليك";
 
     static $STATE_DELIVERED = "تم تسليم الطلب";
 
@@ -22,7 +22,7 @@ abstract class LocalNotificationList
 
     static $UPDATE_ORDER_SUCCESS = "تم تعديل الطلب بنجاح";
 
-    static $UPDATE_ORDER_ERROR_CAPTAIN_IN_STORE = "عذرا لا تستطيع تعديل الطلب , المندوب في المتجر.";
+    static $UPDATE_ORDER_ERROR_CAPTAIN_IN_STORE = "عذرا لا تستطيع تعديل الطلب , الكابتن في المتجر.";
 
     static $CANCEL_ORDER_TITLE = "حذف طلب";
 
@@ -30,6 +30,6 @@ abstract class LocalNotificationList
 
     static $CANCEL_ORDER_ERROR_TIME = "لا يمكن حذف الطلب, لقد تجاوزت الوقت المسموح به للحذف.";
 
-    static $CANCEL_ORDER_ERROR_ACCEPTED = "لا يمكن حذف الطلب, المندوب استلم الطلب.";
+    static $CANCEL_ORDER_ERROR_ACCEPTED = "لا يمكن حذف الطلب, الكابتن استلم الطلب.";
 
 }
