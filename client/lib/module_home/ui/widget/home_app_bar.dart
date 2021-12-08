8a5a542dd423ee7c2463d1d3b0d9904ed3f0ead9
yazan99sh <syria.yazan19@gmail.com>
2021-12-08 14:52:25 +0200

    remove language picker

--- client/lib/module_home/ui/widget/home_app_bar.dart
+++ client/lib/module_home/ui/widget/home_app_bar.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:mandob_moshtarayat/di/di_config.dart';
 import 'package:mandob_moshtarayat/generated/l10n.dart';
 import 'package:mandob_moshtarayat/module_home/ui/widget/catagories_card.dart';
-import 'package:mandob_moshtarayat/module_localization/service/localization_service/localization_service.dart';
 import 'package:mandob_moshtarayat/module_my_notifications/my_notifications_routes.dart';
 import 'package:mandob_moshtarayat/module_search/search_routes.dart';
 import 'package:mandob_moshtarayat/utils/components/costom_search.dart';
@@ -13,7 +11,7 @@ import 'package:mandob_moshtarayat/utils/models/store_category.dart';
 class CustomHomeAppBar extends StatefulWidget {
   final Function(String) categoriesCallback;
   final List<StoreCategoryModel> categories;
-  CustomHomeAppBar(
+  const CustomHomeAppBar(
       {required this.categoriesCallback, required this.categories});
 
   @override
@@ -32,14 +30,12 @@ class _CustomHomeAppBarState extends State<CustomHomeAppBar> {
           Padding(
             padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(right: 8.0, left: 8.0),
                   child: SvgPicture.asset(SvgAsset.LOGO_SVG, width: 35),
                 ),
-                SizedBox(
-                  width: 235,
+                Expanded(
                   child: CustomDeliverySearch(
                     onTap: () {
                       Navigator.pushNamed(context, SearchRoutes.SEARCH_SCREEN);
@@ -48,63 +44,28 @@ class _CustomHomeAppBarState extends State<CustomHomeAppBar> {
                     hintText: S.of(context).searchFor,
                   ),
                 ),
-                Expanded(
-                    child: PopupMenuButton<String>(
-                  itemBuilder: (context) => [
-                    const PopupMenuItem(
-                      value: 'ar',
-                      child: Text('العربية'),
-                    ),
-                    const PopupMenuItem(
-                      value: 'en',
-                      child: Text('English'),
-                    ),
-                  ],
-                  initialValue: getIt<LocalizationService>().getLanguage(),
-                  onSelected: (value) {
-                    getIt<LocalizationService>().setLanguage(value);
-                  },
-                  child: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Theme.of(context).primaryColor),
-                      child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                        getIt<LocalizationService>().getLanguage() == 'ar'
-                              ? 'اب'
-                              : getIt<LocalizationService>().getLanguage(),
-                        style: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                          ))),
-                )),
-                const SizedBox(
+               const SizedBox(
                   width: 8,
                 ),
-                Expanded(
-                    child: GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pushNamed(
-                              MyNotificationsRoutes.MY_NOTIFICATIONS);
-                        },
-                        child: Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Theme.of(context).primaryColor),
-                            child: const Center(
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Icon(Icons.notifications,
-                                      color: Colors.white),
-                                ))))),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushNamed(
+                          MyNotificationsRoutes.MY_NOTIFICATIONS);
+                    },
+                    child: Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Theme.of(context).primaryColor),
+                        child: const Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(Icons.notifications,
+                                  color: Colors.white),
+                            )))),
+                const SizedBox(
+                  width: 8,
+                ), 
               ],
             ),
           ),
