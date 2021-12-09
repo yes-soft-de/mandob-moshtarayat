43a5c5b64416ee6a239a64a0f5e7f29f03571d7c
habbdevsy <alhabebcenter@hotmail.com>
2021-12-09 14:05:06 +0200

    refactor for mandob register.

--- backend/src/Manager/MandobProfileManager.php
+++ backend/src/Manager/MandobProfileManager.php
@@ -4,14 +4,12 @@ namespace App\Manager;
 
 use App\AutoMapping;
 use App\Entity\MandobProfileEntity;
-use App\Entity\UserEntity;
 use App\Manager\UserManager;
 use App\Repository\MandobProfileEntityRepository;
 use App\Request\ActivateMandobAccountByAdminRequest;
 use App\Request\MandobProfileUpdateRequest;
 use App\Request\UserRegisterRequest;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class MandobProfileManager
@@ -20,24 +18,23 @@ class MandobProfileManager
     private $entityManager;
     private $mandobProfileEntityRepository;
     private $userManager;
-    private $encoder;
 
-    public function __construct(AutoMapping $autoMapping, EntityManagerInterface $entityManager,UserPasswordEncoderInterface $encoder,
+    public function __construct(AutoMapping $autoMapping, EntityManagerInterface $entityManager,
                                 MandobProfileEntityRepository $mandobProfileEntityRepository, UserManager $userManager)
     {
         $this->autoMapping = $autoMapping;
         $this->entityManager = $entityManager;
         $this->mandobProfileEntityRepository = $mandobProfileEntityRepository;
         $this->userManager = $userManager;
-        $this->encoder = $encoder;
 
     }
 
     public function mandobRegister(UserRegisterRequest $request)
     {
         $user = $this->userManager->getUserByUserID($request->getUserID());
-        if ($user == null) {
+        if (!$user) {
             $request->setRoles(["ROLE_MANDOB"]);
+
             $userRegister = $this->userManager->createUser($request);
             if($userRegister){
                return $this->createProfile($request, $userRegister);
@@ -55,7 +52,7 @@ class MandobProfileManager
 
         $mandobProfile = $this->mandobProfileEntityRepository->getMandobProfileByMandobID($request->getUserID());
 
-        if ($mandobProfile == null) {
+        if (!$mandobProfile) {
              $mandobProfile = $this->autoMapping->map(UserRegisterRequest::class, MandobProfileEntity::class, $request);
              $mandobProfile->setStatus('inactive');
              $mandobProfile->setMandobID($userRegister->getId());
@@ -63,7 +60,6 @@ class MandobProfileManager
 
              $this->entityManager->persist($mandobProfile);
              $this->entityManager->flush();
-             $this->entityManager->clear();
             }
         return $userRegister;
     }
@@ -72,7 +68,7 @@ class MandobProfileManager
     {
         $mandobProfile = $this->mandobProfileEntityRepository->getMandobProfileByMandobID($user['id']);
 
-        if ($mandobProfile == null) {
+        if (!$mandobProfile) {
             $mandobProfile = $this->autoMapping->map(UserRegisterRequest::class, MandobProfileEntity::class, $request);
 
             $mandobProfile->setStatus('inactive');
@@ -81,67 +77,10 @@ class MandobProfileManager
 
             $this->entityManager->persist($mandobProfile);
             $this->entityManager->flush();
-            $this->entityManager->clear();
         }
 
         return 'user is found';
     }
-//this code is for backup (mandob Register).
-//
-//    public function mandobRegister1(UserRegisterRequest $request)
-//    {
-//        $user = $this->userManager->getUserByUserID($request->getUserID());
-//        if ($user == null) {
-//
-//            $userRegister = $this->autoMapping->map(UserRegisterRequest::class, UserEntity::class, $request);
-//
-//            $user = new UserEntity($request->getUserID());
-//
-//            if ($request->getPassword()) {
-//                $userRegister->setPassword($this->encoder->encodePassword($user, $request->getPassword()));
-//            }
-//
-//            $userRegister->setRoles(["ROLE_MANDOB"]);
-//
-//            $this->entityManager->persist($userRegister);
-//            $this->entityManager->flush();
-//            $this->entityManager->clear();
-//            // Second, create the mandob's profile
-//            $mandobProfile = $this->mandobProfileEntityRepository->getMandobProfileByMandobID($request->getUserID());
-//
-//            if ($mandobProfile == null) {
-//                $mandobProfile = $this->autoMapping->map(UserRegisterRequest::class, MandobProfileEntity::class, $request);
-//
-//                $mandobProfile->setStatus('inactive');
-//                $mandobProfile->setMandobID($userRegister->getId());
-//                $mandobProfile->setMandobName($request->getUserName());
-//
-//                $this->entityManager->persist($mandobProfile);
-//                $this->entityManager->flush();
-//                $this->entityManager->clear();
-//            }
-//            return $userRegister;
-//        }
-//        else
-//        {
-//            $mandobProfile = $this->mandobProfileEntityRepository->getMandobProfileByMandobID($user['id']);
-//
-//            if ($mandobProfile == null)
-//            {
-//                $mandobProfile = $this->autoMapping->map(UserRegisterRequest::class, MandobProfileEntity::class, $request);
-//
-//
-//                $mandobProfile->setStatus('inactive');
-//                $mandobProfile->setMandobID($mandobProfile->getId());
-//                $mandobProfile->setMandobName($request->getUserName());
-//
-//                $this->entityManager->persist($mandobProfile);
-//                $this->entityManager->flush();
-//                $this->entityManager->clear();
-//            }
-//            return "user is found";
-//        }
-//    }
 
     public function updateMandobProfile(MandobProfileUpdateRequest $request)
     {
