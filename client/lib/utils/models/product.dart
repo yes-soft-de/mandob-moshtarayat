33671bff8e9ba550b32b94cb3f67d6afabd03a91
yazan99sh <syria.yazan19@gmail.com>
2021-12-26 13:52:56 +0200

    cart details

--- client/lib/utils/models/product.dart
+++ client/lib/utils/models/product.dart
@@ -1,6 +1,7 @@
 import 'package:mandob_moshtarayat/generated/l10n.dart';
 import 'package:mandob_moshtarayat/module_stores/response/products_by_category.dart';
 import 'package:mandob_moshtarayat/module_stores/response/store_products.dart';
+import 'package:mandob_moshtarayat/utils/response/cost_details_response/cost_details.dart';
 
 class ProductModel {
   late String title;
@@ -10,9 +11,8 @@ class ProductModel {
   String? storeImage;
   String? storeName;
   num? deliveryCost;
-
+  CostDetails? costDetails;
   int? quantity;
-
   ProductModel(
       {required this.title,
       required this.image,
@@ -21,7 +21,8 @@ class ProductModel {
       this.storeName,
       this.deliveryCost,
       this.storeImage,
-      this.quantity = 0});
+      this.quantity = 0,
+      this.costDetails});
 
   List<ProductModel> models = [];
 
@@ -41,7 +42,9 @@ class ProductModel {
           title: element.productName ?? S.current.product,
           image: element.productImage?.image ?? '',
           price: element.productPrice ?? 0,
-          id: element.id ?? -1));
+          id: element.id ?? -1,
+          costDetails: element.costDetails
+          ));
     });
   }
 
@@ -52,7 +55,9 @@ class ProductModel {
           image: element.productImage?.image ??
               'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
           id: element.id ?? -1,
-          price: element.productPrice ?? 0));
+          price: element.productPrice ?? 0,
+          costDetails: element.costDetails
+          ));
     });
   }
 
