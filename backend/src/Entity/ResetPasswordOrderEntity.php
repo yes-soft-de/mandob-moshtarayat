55e0f0ffb3de71ef3d8e91792fcc6c0cd4113a60
ralsalloum <ramisalloum12@hotmail.com>
2021-12-27 23:01:52 +0200

    ResetPasswordOrderEntity was added

---
+++ backend/src/Entity/ResetPasswordOrderEntity.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResetPasswordOrderEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity(repositoryClass=ResetPasswordOrderEntityRepository::class)
+ */
+class ResetPasswordOrderEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userID;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $code;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserID(): ?string
+    {
+        return $this->userID;
+    }
+
+    public function setUserID(string $userID): self
+    {
+        $this->userID = $userID;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
