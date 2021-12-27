4f93eef9061202d0123d5613393d029c5c2bbb49
yazan99sh <syria.yazan19@gmail.com>
2021-12-27 16:43:48 +0200

    country code

---
+++ client/lib/consts/country_code.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+class CountryCode{
+ //Dev
+//  static const String COUNTRY_CODE='963';
+
+  //PRO
+  static const String COUNTRY_CODE_KSA='966'  ;
+  static const String COUNTRY_CODE_SY='963'  ;
+  
+}
