a8f81928cdc6ca09ac5623a844a8463a7bf97d52
yazan99sh <syria.yazan19@gmail.com>
2021-12-30 21:12:12 +0200

    custom bottom sheet

--- client/lib/module_main/ui/screen/main_screen.dart
+++ client/lib/module_main/ui/screen/main_screen.dart
@@ -12,6 +12,7 @@ import 'package:mandob_moshtarayat/module_report/request/custom_product_request.
 import 'package:mandob_moshtarayat/module_report/service/report_service.dart';
 import 'package:mandob_moshtarayat/module_upload/service/image_upload/image_upload_service.dart';
 import 'package:mandob_moshtarayat/utils/customIcon/mandob_icons_icons.dart';
+import 'package:mandob_moshtarayat/utils/effect/hidder.dart';
 import 'package:mandob_moshtarayat/utils/helpers/custom_flushbar.dart';
 
 @injectable
@@ -24,7 +25,8 @@ class _MainScreenState extends State<MainScreen> {
   int selectedPage = 0;
   PageController homeController = PageController(initialPage: 0);
   bool flagPageIndex = true;
-
+  final GlobalKey<ScaffoldState> bottomSheet = GlobalKey<ScaffoldState>();
+  bool fade = false;
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context)?.settings.arguments;
@@ -33,92 +35,116 @@ class _MainScreenState extends State<MainScreen> {
       homeController = PageController(initialPage: args);
       flagPageIndex = false;
     }
-    return Scaffold(
-      body: SafeArea(
-        bottom: true,
-        top: true,
-        child: Stack(
-          children: [
-            PageView(
-              physics: const NeverScrollableScrollPhysics(),
-              controller: homeController,
-              onPageChanged: (index) {
-                selectedPage = index;
-                setState(() {});
-                homeController.animateToPage(index,
-                    duration: const Duration(milliseconds: 15),
-                    curve: Curves.linear);
-              },
-              children: [
-                getIt<HomeScreen>(),
-                getIt<MyOrdersScreen>(),
-                getIt<CartScreen>(),
-                getIt<AccountScreen>()
-              ],
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 65,
-                child: Scaffold(
-                  backgroundColor: Colors.transparent.withOpacity(0.0),
-                  floatingActionButton: FloatingActionButton(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    child: const Icon(
-                      MandobIcons.logo,
-                      color: Colors.white,
-                      size: 30,
+    return WillPopScope(
+      onWillPop: () async {
+        if (fade) {
+          fade = false;
+          setState(() {});
+        }
+        return true;
+      },
+      child: Scaffold(
+        key: bottomSheet,
+        body: SafeArea(
+          bottom: true,
+          top: true,
+          child: Stack(
+            children: [
+              PageView(
+                physics: const NeverScrollableScrollPhysics(),
+                controller: homeController,
+                onPageChanged: (index) {
+                  selectedPage = index;
+                  setState(() {});
+                  homeController.animateToPage(index,
+                      duration: const Duration(milliseconds: 15),
+                      curve: Curves.linear);
+                },
+                children: [
+                  getIt<HomeScreen>(),
+                  getIt<MyOrdersScreen>(),
+                  getIt<CartScreen>(),
+                  getIt<AccountScreen>()
+                ],
+              ),
+              Hider(
+                  active: fade,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (fade) {
+                        fade = false;
+                        Navigator.of(context).pop();
+                        setState(() {});
+                      }
+                    },
+                    child: Container(
+                      color: Colors.black38,
                     ),
-                    onPressed: () {
-                      showModalBottomSheet<void>(
-                        shape: const RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(25)),
-                        ),
-                        backgroundColor: Colors.transparent.withOpacity(0),
-                        context: context,
-                        builder: (BuildContext context) {
-                          return OuterOrderBottomSheet(
-                            callback: (name, extraInfo, image) {
-                              Navigator.of(context).pop();
-                              CustomFlushBarHelper.createSuccess(
-                                      title: S.current.warnning,
-                                      message: S.current.yourRequestSent,
-                                      timeout: 1)
-                                  .show(context);
-                              createCustomProduct(name,extraInfo,image);
-                            },
-                          );
-                        },
-                      );
-                    },
-                  ),
-                  floatingActionButtonLocation:
-                      FloatingActionButtonLocation.centerDocked,
-                  bottomNavigationBar: AnimatedBottomNavigationBar(
-                    inactiveColor: Theme.of(context).disabledColor,
-                    activeColor: Theme.of(context).primaryColor,
-                    gapLocation: GapLocation.center,
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                    notchSmoothness: NotchSmoothness.defaultEdge,
-                    icons: const [
-                      Icons.home,
-                      Icons.list,
-                      Icons.shopping_cart_rounded,
-                      Icons.person_pin
-                    ],
-                    onTap: (int index) {
-                      selectedPage = index;
-                      homeController.animateToPage(index,
-                          duration: const Duration(milliseconds: 15),
-                          curve: Curves.linear);
-                    },
-                    activeIndex: selectedPage,
+                  )),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  height: 65,
+                  child: Scaffold(
+                    backgroundColor: Colors.transparent.withOpacity(0.0),
+                    floatingActionButton: FloatingActionButton(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      child: const Icon(
+                        MandobIcons.logo,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        fade = true;
+                        bottomSheet.currentState?.showBottomSheet(
+                            (context) => OuterOrderBottomSheet(
+                                  callback: (name, extraInfo, image) {
+                                    Navigator.of(context).pop();
+                                    setState(() {});
+                                    CustomFlushBarHelper.createSuccess(
+                                            title: S.current.warnning,
+                                            message: S.current.yourRequestSent,
+                                            timeout: 1)
+                                        .show(context);
+                                    createCustomProduct(name, extraInfo, image);
+                                  },
+                                ),
+                            elevation: 5,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(25)),
+                            ));
+                        setState(() {});
+                      },
+                    ),
+                    floatingActionButtonLocation:
+                        FloatingActionButtonLocation.centerDocked,
+                    bottomNavigationBar: AnimatedBottomNavigationBar(
+                      inactiveColor: Theme.of(context).disabledColor,
+                      activeColor: Theme.of(context).primaryColor,
+                      gapLocation: GapLocation.center,
+                      backgroundColor:
+                          Theme.of(context).scaffoldBackgroundColor,
+                      notchSmoothness: NotchSmoothness.defaultEdge,
+                      icons: const [
+                        Icons.home,
+                        Icons.list,
+                        Icons.shopping_cart_rounded,
+                        Icons.person_pin
+                      ],
+                      onTap: (int index) {
+                        selectedPage = index;
+                        homeController.animateToPage(index,
+                            duration: const Duration(milliseconds: 15),
+                            curve: Curves.linear);
+                      },
+                      activeIndex: selectedPage,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- client/lib/module_main/widget/bottom_sheet.dart
+++ client/lib/module_main/widget/bottom_sheet.dart
@@ -26,8 +26,9 @@ class _OuterOrderBottomSheetState extends State<OuterOrderBottomSheet> {
       color: Theme.of(context).disabledColor,
       fontSize: 14,
     );
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return SingleChildScrollView(
+      physics:
+          const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
       child: Container(
         constraints: const BoxConstraints(minHeight: 400, maxHeight: 475),
         decoration: BoxDecoration(
@@ -36,129 +37,127 @@ class _OuterOrderBottomSheetState extends State<OuterOrderBottomSheet> {
         child: Center(
           child: Form(
             key: _orderKey,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  // title
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        right: 16.0, left: 16, bottom: 16),
-                    child: Center(
-                      child: Text(
-                        S.current.enterProductInfo,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                    ),
-                  ),
-                  // product Name
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 16, right: 16, bottom: 8),
-                    child: CustomFormField(
-                      hintText: S.current.productName,
-                      controller: productNameController,
-                      hintStyle: hintStyle,
-                    ),
-                  ),
-                  // Extra Info
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16.0, right: 16, bottom: 16),
-                    child: CustomFormField(
-                      hintText: S.current.extraDetails,
-                      controller: detailsController,
-                      maxLines: 3,
-                      hintStyle: hintStyle,
-                    ),
-                  ),
-                  // upload image
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(right: 16.0, left: 16, bottom: 8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                // title
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 16.0, left: 16, bottom: 16),
+                  child: Center(
                     child: Text(
-                      S.current.uploadImageIfyouHave,
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                      S.current.enterProductInfo,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16.0, left: 16),
-                    child: GestureDetector(
-                      onTap: () {
-                        ImagePicker.platform
-                            .pickImage(source: ImageSource.gallery)
-                            .then((value) {
-                          imagePath = value?.path;
-                          setState(() {});
-                        });
-                      },
-                      child: SizedBox(
-                        width: 70,
-                        height: 70,
-                        child: Checked(
-                            checked: imagePath != null,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(18),
-                                  color: Theme.of(context).backgroundColor),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.cloud_upload_sharp,
-                                    color: Theme.of(context).disabledColor,
-                                  ),
-                                  Text(
-                                    S.current.pressHere,
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context).disabledColor),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            checkedWidget: ClipRRect(
-                                borderRadius: BorderRadius.circular(18),
-                                child: Image.file(
-                                  File(imagePath ?? ''),
-                                  fit: BoxFit.cover,
-                                ))),
-                      ),
-                    ),
-                  ), // send
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
+                ),
+                // product Name
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 16, right: 16, bottom: 8),
+                  child: CustomFormField(
+                    hintText: S.current.productName,
+                    controller: productNameController,
+                    hintStyle: hintStyle,
+                  ),
+                ),
+                // Extra Info
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 16.0, right: 16, bottom: 16),
+                  child: CustomFormField(
+                    hintText: S.current.extraDetails,
+                    controller: detailsController,
+                    maxLines: 3,
+                    hintStyle: hintStyle,
+                  ),
+                ),
+                // upload image
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 16.0, left: 16, bottom: 8),
+                  child: Text(
+                    S.current.uploadImageIfyouHave,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0, left: 16),
+                  child: GestureDetector(
+                    onTap: () {
+                      ImagePicker.platform
+                          .pickImage(source: ImageSource.gallery)
+                          .then((value) {
+                        imagePath = value?.path;
+                        setState(() {});
+                      });
+                    },
                     child: SizedBox(
-                      width: double.maxFinite,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        )),
-                        onPressed: () {
-                          if (_orderKey.currentState?.validate() == true) {
-                            widget.callback(productNameController.text,
-                                detailsController.text, imagePath);
-                          }
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            S.current.send,
-                            style: const TextStyle(
-                              color: Colors.white,
+                      width: 70,
+                      height: 70,
+                      child: Checked(
+                          checked: imagePath != null,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                                color: Theme.of(context).backgroundColor),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.cloud_upload_sharp,
+                                  color: Theme.of(context).disabledColor,
+                                ),
+                                Text(
+                                  S.current.pressHere,
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context).disabledColor),
+                                ),
+                              ],
                             ),
+                          ),
+                          checkedWidget: ClipRRect(
+                              borderRadius: BorderRadius.circular(18),
+                              child: Image.file(
+                                File(imagePath ?? ''),
+                                fit: BoxFit.cover,
+                              ))),
+                    ),
+                  ),
+                ), // send
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: double.maxFinite,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      )),
+                      onPressed: () {
+                        if (_orderKey.currentState?.validate() == true) {
+                          widget.callback(productNameController.text,
+                              detailsController.text, imagePath);
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          S.current.send,
+                          style: const TextStyle(
+                            color: Colors.white,
                           ),
                         ),
                       ),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
