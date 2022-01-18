639ee399ac2e3ffb8679d09f7c3ce0c9c1e1b8e2
yazan99sh <syria.yazan19@gmail.com>
2022-01-18 20:54:45 +0200

    payment into production

--- client/lib/utils/helpers/payment_portal.dart
+++ client/lib/utils/helpers/payment_portal.dart
@@ -127,7 +127,7 @@ class _PaymentsPortalState extends State<PaymentsPortal> {
           // select payments you need to show [Default is all, and you can choose between WEB-CARD-APPLEPAY ]
           paymentType: PaymentType.ALL,
           // Transaction mode
-          sdkMode: SDKMode.Sandbox);
+          sdkMode: SDKMode.Production);
     } on PlatformException {
       // platformVersion = 'Failed to get platform version.';
     }
